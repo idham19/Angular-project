36d9d3af62a43fa14b640e3165f8f2c68d7b9ccb
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-17 15:40:08 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { DeviceDetailsComponent } from './device-details/device-details.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SiteHeaderComponent,
+    DeviceDetailsComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/device-details/device-details.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-device-details',
+  templateUrl: './device-details.component.html',
+  styleUrls: ['./device-details.component.css']
+})
+export class DeviceDetailsComponent {
+@Input() device:any;
+}
